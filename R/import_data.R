f5e05a632ba0410f558f88868d9f0634f77e07b9
clemdelrevo <clement.delamare@live.fr>
2023-07-25 09:06:24 +0400

    debug import_data :space_invader:

--- R/import_data.R
+++ R/import_data.R
@@ -14,9 +14,9 @@ import_line_function <- function(list_line_files) {
     
     message(paste(site, annee))
     
-    if(file_format == "xls") {
-    segments <- as.data.frame(readxl::read_xls(i, range = "R42C1:R51C8", col_names = FALSE))
-    } else if(file_format == "xlsx") {
+    if (file_format == "xls") {
+      segments <- as.data.frame(readxl::read_xls(i, range = "R42C1:R51C8", col_names = FALSE))
+    } else if (file_format == "xlsx") {
       segments <- as.data.frame(readxl::read_xlsx(i, range = "R42C1:R51C8", col_names = FALSE))
     }  
     
@@ -24,12 +24,12 @@ import_line_function <- function(list_line_files) {
     colnames(segments) <- c("substrat", "t1", "t2", "t3", "t4")
     
   
-    if(any(is.na(apply(segments[, names(segments) %in% c("t1", "t2", "t3", "t4")], 2, sum)))) {
+    if (any(is.na(apply(segments[, names(segments) %in% c("t1", "t2", "t3", "t4")], 2, sum)))) {
         
-      if(file_format == "xls") {
-          segments <- as.data.frame(readxl::read_xls(i, range = "R52C1:R61C8", col_names = FALSE))
-      } else if(file_format == "xlsx") {
-          segments <- as.data.frame(readxl::read_xlsx(i, range = "R52C1:R61C8", col_names = FALSE))
+      if (file_format == "xls") {
+        segments <- as.data.frame(readxl::read_xls(i, range = "R52C1:R61C8", col_names = FALSE))
+      } else if (file_format == "xlsx") {
+        segments <- as.data.frame(readxl::read_xlsx(i, range = "R52C1:R61C8", col_names = FALSE))
       }
         
       segments <- segments[, c(1,2,4,6,8)]
@@ -64,12 +64,12 @@ import_line_function <- function(list_line_files) {
 
 import_belt_function <- function(list_belt_files){
   
-  data_belt <- data.frame(do.call(rbind,lapply(list_belt_files, function(i) {
+  data_belt <- data.frame(do.call(rbind, lapply(list_belt_files, function(i) {
    
-    #i = "data/14-LONGONI/16-0613/2016b_longoni_belt.xls"
+    #i = "data/mayotte/14-LONGONI/16-0613/2016b_longoni_belt.xls"
     
     fish_name   <- c("Butterflyfish", "Haemulidae", "Snapper", "Barramundi cod", "Humphead wrasse", "Bumphead parrot", "Parrotfish", "Moray eel", "Grouper total")
-    invert_name <- c("Banded coral shrimp", "Diadema", "Pencil urchin", "Collector urchin", "Sea cucumber", "Crown-of-thorns", "Triton", "Lobster", "Giant clam total")
+    invert_name <- c("Banded coral shrimp", "Diadema urchin", "Pencil urchin", "Collector urchin", "Sea cucumber", "Crown-of-thorns", "Triton", "Lobster", "Giant clam total")
     
     file_name   <- basename(i)
     annee       <- gsub("[^0-9]", "", file_name)
@@ -79,25 +79,30 @@ import_belt_function <- function(list_belt_files){
     
     message(paste(site, annee))
     
-    if(file_format == "xls") {
+    # import from old format of data sheet
+    if (file_format == "xls") {
       fish   <- as.data.frame(readxl::read_xls(i, range = "R10C1:R25C5" , col_names = FALSE))
       invert <- as.data.frame(readxl::read_xls(i, range = "R29C1:R45C5" , col_names = FALSE))
     
-    } else if(file_format == "xlsx") {
+    } else if (file_format == "xlsx") {
       fish     <- as.data.frame(readxl::read_xlsx(i, range = "R10C1:R25C5", col_names = FALSE))
       invert   <- as.data.frame(readxl::read_xlsx(i, range = "R29C1:R45C5" , col_names = FALSE))
       
     }
     
     colnames(fish) <- c("fish", "t1", "t2", "t3", "t4")
+    colnames(invert) <- c("invert", "t1", "t2", "t3", "t4")
+    invert$invert[invert$invert == "Diadema"] <- "Diadema urchin"
     
-  if(all(fish$fish[fish$fish %in% fish_name] == fish_name) == FALSE) {
+    # If there is no match in taxon names, try to import from other format of data sheet  
+    if (length(fish$fish[fish$fish %in% fish_name]) != length(fish_name) |
+      length(invert$invert[invert$invert %in% invert_name]) != length(invert_name)) {
       
-      if(file_format == "xls") {
+      if (file_format == "xls") {
         fish   <- as.data.frame(readxl::read_xls(i, range = "R11C1:R25C5" , col_names = FALSE))
         invert <- as.data.frame(readxl::read_xls(i, range = "R32C1:R48C5" , col_names = FALSE))
         
-      } else if(file_format == "xlsx") {
+      } else if (file_format == "xlsx") {
         fish     <- as.data.frame(readxl::read_xlsx(i, range = "R11C1:R25C5", col_names = FALSE))
         invert   <- as.data.frame(readxl::read_xlsx(i, range = "R32C1:R48C5" , col_names = FALSE))
         
@@ -107,22 +112,20 @@ import_belt_function <- function(list_belt_files){
     colnames(invert) <- c("invert", "t1", "t2", "t3", "t4")
     
     fish$fish[fish$fish == "Total # grouper observed"] <- "Grouper total"
-    invert$invert[invert$invert == "Total # giant clams observed"] <- "Giant claim total"
+    invert$invert[invert$invert == "Total # giant clams observed"] <- "Giant clam total"
   
     }
     
-    if(any(is.na(fish[fish$fish %in% fish_name, names(fish) %in% c("t1", "t2", "t3", "t4")]))) {
-      
-      message("Survey of ", site, " in ", annee, " was not included because of NA values")
-      return(NULL)
-      }
-    
     # fish ---------------------------------------------------------------------
+    if (any(fish$fish[fish$fish %in% fish_name] == fish_name) == FALSE) {
+      stop(message(paste0("Data import problem. Please chech data sheet survey of ", site, " ", annee)))
+    }
     fish <- fish[fish$fish %in% fish_name, ]
     
     fish$fish[fish$fish == "Grouper total"] <- "Grouper"
     fish$fish <- tolower(fish$fish)
     fish$fish <- gsub(" ", "_", fish$fish)
+    fish_name <- fish$fish
       
     fish_gather <- tidyr::gather(fish, transect, value, -fish)
     fish_spread <- tidyr::spread(fish_gather, fish, value)
@@ -132,9 +135,17 @@ import_belt_function <- function(list_belt_files){
       
     fish <- data.frame(cbind(site = n_site, annee = n_annee, fish_spread))
     fish$annee <- as.integer(fish$annee)
+    
+    if (any(is.na(fish[, names(fish) %in% fish_name]))) {
+      
+      message("Fish survey of ", site, " in ", annee, " included NA values. Please check data.")
+  
+    }
       
     # invert -------------------------------------------------------------------
-    
+    if (any(invert$invert[invert$invert %in% invert_name] == invert_name) == FALSE) {
+      stop(message(paste0("Data import problem. Please chech data sheet survey of ", site, " ", annee)))
+    }
     invert <- invert[invert$invert %in% invert_name, ]
     
     invert$invert[invert$invert == "Giant clam total"] <- "Giant clam"
@@ -150,13 +161,21 @@ import_belt_function <- function(list_belt_files){
       
     invert <- data.frame(cbind(site = n_site, annee = n_annee, invert_spread))
     invert$annee <- as.integer(invert$annee)
+    
+    if (any(is.na(invert[, names(invert) %in% invert_name]))) {
       
+      message("Invert survey of ", site, " in ", annee, " included NA values. Please check data.")
+      
+    }
+    
     return(list(fish = fish, invert = invert))
       
   })))
-  data_belt$fish <- na.omit(data_belt$fish)
+  
   data_fish   <- data.frame(do.call(rbind, data_belt$fish))
-  data_invert <- do.call(rbind, data_belt$invert)
+  data_fish   <- na.omit(data_fish)
+  data_invert <- data.frame(do.call(rbind, data_belt$invert))
+  data_invert <- na.omit(data_invert)
   
   return(list(data_fish = data_fish, data_invert = data_invert))
   
@@ -164,7 +183,7 @@ import_belt_function <- function(list_belt_files){
 
 import_line_may <- function(list_line_may) {
   
-  #targets::tar_load(list_line_files)
+  #targets::tar_load(list_line_may)
   
   import_line_function(list_line_files = list_line_may)
   
@@ -172,7 +191,7 @@ import_line_may <- function(list_line_may) {
 
 import_belt_may <- function(list_belt_may) {
   
-  #targets::tar_load(list_belt_files)
+  #targets::tar_load(list_belt_may)
   
   import_belt_function(list_belt_files = list_belt_may)
   
--- R/station_sheet.R
+++ R/station_sheet.R
@@ -1,22 +1,142 @@
-fish_station <- function(data_fish) {
+# Generate barplot to have an overview of fish or 
+# invert mean abondance in each station ---------------------------------------------
+
+stat_bar_function <- function(data, taxon_name, color) {
   
-  fish_station <- tidyr::gather(data = data_fish, fish, abondance, -site, -annee, -transect)
+  #targets::tar_load(data_fish)
+
+  taxon_abondance <- tidyr::gather(data = data, taxon, abondance, -site, -annee, -transect, -reef_type)
+  max_year        <- tapply(taxon_abondance$annee, taxon_abondance$site, max)
+  taxon_abondance <- taxon_abondance[taxon_abondance$annee %in% max_year, ]
   
+  taxon_abondance <- setNames(lapply(levels(as.factor(taxon_abondance$site)), function(site) {
   
-  max_year <- tapply(fish_station$annee, fish_station$site, max)
+    #site = "pes.onze"
+    taxon_station <- taxon_abondance[taxon_abondance$site == site, ]  
+    taxon_station <-  taxon_station |>
+      dplyr::group_by(site, taxon) |>
+      dplyr::summarise(mean_abondance = mean(abondance),
+                       st_error_abondance = plotrix::std.error(abondance))
+    
+    taxon_station <- taxon_station[taxon_station$mean_abondance != 0, ]
+    taxon_name    <- taxon_name 
+    color         <- color 
+    taxon_station <- taxon_station[order(taxon_station$taxon), ]
+    
+    taxon_station$taxon_name <- taxon_name[names(taxon_name) %in% taxon_station$taxon]
+    taxon_station$color      <- color[names(color) %in% taxon_station$taxon]
+    
+    taxon_station <- taxon_station[order(taxon_station$mean_abondance), ]
+    taxon_station$color <- taxon_station$color |>
+      forcats::fct_inorder()|>
+      forcats::fct_rev()
+    taxon_station$taxon_name<- taxon_station$taxon_name |>
+      forcats::fct_inorder()|>
+      forcats::fct_rev()
+    
+    ggplot2::ggplot()+
+      ggplot2::geom_col(data= taxon_station, ggplot2::aes(x = taxon_name, y = mean_abondance, fill = taxon_name), linewidth = 0.7, color = "black")+
+      ggplot2::geom_errorbar(data = taxon_station, ggplot2::aes(x = taxon_name, ymin = mean_abondance - st_error_abondance, ymax = mean_abondance + st_error_abondance),
+                             linewidth = 0.5, width = 0.2)+
+      ggplot2::coord_flip()+
+      ggplot2::scale_fill_manual(values = levels(as.factor(taxon_station$color)))+
+      ggplot2::theme_bw()+
+      ggplot2::ylab(bquote("Abondance moyenne (nb/100"*m^2*")"))+
+      ggplot2::theme(axis.title.y = ggplot2::element_blank(), legend.position = "none",
+                     plot.subtitle = ggplot2::element_text(face = "bold", size = 12),
+                     axis.text = ggplot2::element_text(face = "bold", size = 12))
+      
+    
+  }), levels(as.factor(taxon_abondance$site)))
+
+  return(taxon_abondance)
   
-  fish_station <- fish_station[fish_station$annee %in% max_year, ]
+}
   
-  fish_station <-  fish_station |>
-    dplyr::group_by(site, annee, fish) |>
-    dplyr::summarise(mean_abondance = mean(abondance),
-                     sd_abondance = sd(abondance))
+# Generate camenbert to have an overview of mean substrat cover in each station 
+
+camenbert_function <- function(data, color) {
+
+  data_line <- tidyr::gather(data = data, substrat, cover, -site, -annee, -transect)
+  max_year  <- tapply(data_line$annee, data_line$site, max)
+  data_line <- data_line[data_line$annee %in% max_year, ]
+  data_line$cover <- (data_line$cover * 100) / 40
+
+  substrat <- setNames(lapply(levels(as.factor(data_line$site)), function(site) {
+    
+    #site = "tzoundzou"
+    message(site)
+    
+    data_line_site <- data_line[data_line$site == site, ]
+    data_line_site <- data_line_site |>
+      dplyr::group_by(substrat) |>
+      dplyr::summarise(mean_cover = mean(cover)) |>
+      dplyr::arrange(dplyr::desc(substrat))
+    data_line_site <- data_line_site[data_line_site$mean_cover != 0, ]
+    
+    data_line_site$lab.ypos <- cumsum(data_line_site$mean_cover) - 0.5 * data_line_site$mean_cover 
+    
+    color = color
+    
+    data_line_site <- data_line_site[order(data_line_site$substrat), ]
+    data_line_site$color <- color[names(color) %in% data_line_site$substrat]
+    data_line_site$color <- data_line_site$color |>
+      forcats::fct_inorder()
   
-  fish_station_pes.2 <- fish_station[fish_station$site == "pes.deux", ]
+    ggplot2::ggplot(data_line_site, ggplot2::aes(x = "", y = mean_cover, fill = substrat)) +
+      ggplot2::geom_bar(width = 1, stat = "identity", color = "#000000") +
+      ggplot2::coord_polar("y", start = 0) +
+      ggplot2::theme_classic()+
+      ggplot2::labs(fill = "Substrat (%)")+
+      ggplot2::scale_fill_manual(values = as.vector(data_line_site$color))+
+      ggplot2::geom_text(ggplot2::aes(y = lab.ypos, label = round(mean_cover, 1)),
+                         color = "black", size=5, fontface = "bold", check_overlap = TRUE)+
+      ggplot2::theme(axis.text = ggplot2::element_blank(),
+                     axis.title = ggplot2::element_blank(),
+                     axis.line = ggplot2::element_blank(),
+                     axis.ticks = ggplot2::element_blank(),
+                     legend.margin = ggplot2::margin(l = -25, unit = "pt"),
+                     legend.title = ggplot2::element_text(face = "bold"))
+    
   
+    }), levels(as.factor(data_line$site)))
   
-  ggplot2::ggplot()+
-    ggplot2::geom_col(data= fish_station_pes.2, ggplot2::aes(x = fish, y = mean_abondance, fill = fish))+
-    ggplot2::coord_flip()
+  return(substrat)
+    
+}
+
+susbtrat_may_station <- function(data_line_may) {
+  
+  #targets::tar_load(data_line_may)
+  
+  camenbert_function(data = data_line_may, color = c(HC = "#FF6666", NIA = "#99FFCC", OT = "#333333", RB = "#CCCCCC", 
+                                                     RC = "#CC6633", RKC = "#FFFFFF", SC = "#CC0033", SD = "#FFFF99", 
+                                                     SI = "#999900", SP = "#3399CC"))
+  
+}
+
+fish_may_station <- function(data_fish) {
+  
+ stat_bar_function(data = data_fish, 
+                   taxon_name = c(bumphead_parrot = "Perroquet à bosse", butterflyfish = "Chaetodontidae", haemulidae = "Haemulidae",
+                                  humphead_wrasse = "Napoléon", grouper = "Serranidae", moray_eel = "Muraenidae",
+                                  parrotfish = "Scarinae", snapper = "Lutjanidae"),
+                   color =      c(bumphead_parrot = "#336633", butterflyfish ="#FFFF66", haemulidae = "#666666",
+                                  humphead_wrasse = "#66FF99", grouper = "#CC6666", moray_eel = "#6600CC",
+                                  parrotfish = "#006600", snapper = "#003366"))
+                  
+}
+
+invert_may_station <- function(data_invert) {
+  
+  stat_bar_function(data = data_invert,
+                    taxon_name = c(banded_coral_shrimp = "Crevette à bande", diadema_urchin = "Oursin diadème",
+                                   pencil_urchin = "Oursin crayon", collector_urchin = "Oursin collecteur", 
+                                   sea_cucumber = "Holothuroidea", crown_of_thorns = "Acanthasteridae", 
+                                   triton = "Triton", lobster = "Langouste", giant_claim = "Bénitier"),
+                    color = c(banded_coral_shrimp = "#996633", diadema_urchin = "#000000",
+                              pencil_urchin = "#993333", collector_urchin = "#660099", 
+                              sea_cucumber = "#FFCC99", crown_of_thorns = "#CC0033", 
+                              triton = "#CCCC33", lobster = "#FFCCFF", giant_claim = "#6699FF"))
   
 }
